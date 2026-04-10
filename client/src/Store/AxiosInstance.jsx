51ea6ab4aa45fb3f1ef934d8d8ad5abb78ec136b
zeeshan-890 <zzabbaskhan830@gmail.com>
2026-04-10 10:50:55 +0500

    Add environment configuration files and update Axios base URL

--- client/src/Store/AxiosInstance.jsx
+++ client/src/Store/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://ecommerceserver-09a3048c55bb.herokuapp.com/', // set your base URL here
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, // set your base URL here
   withCredentials: true, // send cookies (optional)
 });
 
